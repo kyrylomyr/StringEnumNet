96b4fd9a25a529fc65d1110054926869360db49a
kyrylomyr <1252396+kyrylomyr@users.noreply.github.com>
2020-05-31 08:54:29 +0200

    Implemented first draft

---
+++ StringEnumNet/StringEnum.Instance.cs
@@ -0,0 +1,38 @@
+﻿namespace StringEnumNet
+{
+    public partial class StringEnum<T>
+        where T : StringEnum<T>, new()
+    {
+        private string value;
+
+        public override string ToString()
+        {
+            return value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+            
+            return value == ((StringEnum<T>)obj).value;
+        }
+
+        public override int GetHashCode()
+        {
+            return value != null ? value.GetHashCode() : 0;
+        }
+    }
+}
---
+++ StringEnumNet/StringEnum.Static.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringEnumNet
+{
+    public partial class StringEnum<T>
+    {
+        private static readonly HashSet<string> DefinedValues = new HashSet<string>();
+        
+        protected static T Define(string value)
+        {
+            DefinedValues.Add(value);
+            return new T { value = value };
+        }
+        
+        public static bool IsDefined(string value)
+        {
+            return DefinedValues.Contains(value);
+        }
+
+        public static T Parse(string value)
+        {
+            if (IsDefined(value))
+            {
+                return new T { value = value };
+            }
+            
+            throw new ArgumentOutOfRangeException(
+                nameof(value), $"String enum value '{value}' is not defined in the {typeof(T)}");
+        }
+
+        public static bool TryParse(string value, out string stringEnum)
+        {
+            if (IsDefined(value))
+            {
+                stringEnum = new T { value = value };
+                return true;
+            }
+
+            stringEnum = null;
+            return false;
+        }
+        
+        public static implicit operator string(StringEnum<T> stringEnum)
+        {
+            return stringEnum.ToString();
+        }
+        
+        public static explicit operator StringEnum<T>(string value)
+        {
+            return Parse(value);
+        }
+    }
+}
